233d83114cb4054bc52434af030424fe46653896
NotBello <venujan42@gmail.com>
2024-02-25 19:49:07 +0530

    Add Python3 1000 Port Scanner Code

---
+++ thousandPortScan.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/python
+
+# Using Python 3
+# Created Venujan Malaiyandi 
+# Second Iteration of a simple port scanner
+# Converting the existing codebase for single port scanner into thousand port scanner
+# To check for a thousand ports
+# Using socket library 
+
+
+# importing libraries 
+import socket
+from termcolor import colored
+
+# Create socket objects with IPv4 and TCP protocol
+socketA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Set Defualt Time as 1 second
+socket.setdefaulttimeout(1)
+
+# Declaring host and port details
+host = input("[*] Enter The Host To Scan: ")
+
+# Defining portscanner function
+def scanPort(port):
+	if socketA.connect_ex((host, port)):
+		print(colored("[!] Port %d is closed" % (port), 'red'))
+	else:
+		print(colored("[+] Port %d is open" % (port), 'green'))
+
+for port in range(1,1000):
+	scanPort(port)
